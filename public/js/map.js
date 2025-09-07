03fcb84c857acbf36077079e7ebbe6fb2f273ab3
pallavi221 <pallavibhatt275@gmail.com>
2025-09-07 20:01:58 +0530

    map

--- public/js/map.js
+++ public/js/map.js
@@ -1,21 +1,28 @@
+// Make sure dotenv is loaded if using Node
+if (typeof window === "undefined") {
+  require('dotenv').config();
+}
 
-      mapboxgl.accessToken=mapToken;
+// Get the token from environment variables
+const mapToken = process.env.MAP_token;
 
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center:listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
-         
-    });
+// Only run this in the browser
+if (typeof window !== "undefined") {
+  mapboxgl.accessToken = mapToken;
 
-  //   new mapboxgl.Marker({ color: 'red' })
-  // .setLngLat(coordinates)
-  // .addTo(map);
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: listing.geometry.coordinates, // [lng, lat]
+    zoom: 9
+  });
 
- const marker = new mapboxgl.Marker({color:'green'})
-        .setLngLat(listing.geometry.coordinates)
-        .setPopup(new mapboxgl.Popup({offset:25 })
-    .setHTML(`<h4>${listing.location}</h4><p>Exact Location provided after booking!</p>`)
-    .setMaxWidth("300px"))
-        .addTo(map);
+  const marker = new mapboxgl.Marker({ color: 'green' })
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(
+      new mapboxgl.Popup({ offset: 25 })
+        .setHTML(`<h4>${listing.location}</h4><p>Exact Location provided after booking!</p>`)
+        .setMaxWidth("300px")
+    )
+    .addTo(map);
+}
